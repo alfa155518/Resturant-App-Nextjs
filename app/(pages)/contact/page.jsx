f42edc1210c83b4d9faeee6797d08cabab6e7ebe
Ahmed Hassop <Ahmed Hassob>
2025-03-31 03:51:17 +0200

    add contact page

--- app/(pages)/contact/page.jsx
+++ app/(pages)/contact/page.jsx
@@ -92,7 +92,6 @@ export default function Contact() {
             alt="Contact Us"
             priority
             fill
-            style={{ objectFit: 'cover' }}
             className={styles.headerImage}
           />
         </div>
