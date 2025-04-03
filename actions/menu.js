081b81d3185c975ad72ca87d4b06a7b6d42d4fba
Ahmed Hassop <Ahmed Hassob>
2025-04-03 20:43:35 +0200

    connect api with menu page to show dishes

--- actions/menu.js
+++ actions/menu.js
@@ -5,7 +5,7 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 // menu Dishes
 export async function getMenu(dishesNumber = 1) {
   const res = await fetch(`${apiUrl}/menu?page=${dishesNumber}`, {
-    cache: 'force-cache'
+    // cache: 'force-cache'
   });
   if (!res.ok) {
     console.log(res)
