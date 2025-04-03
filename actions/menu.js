aca48ed30596484e2500516a3c0ef4ed6f920e48
Ahmed Hassop <Ahmed Hassob>
2025-04-03 22:04:37 +0200

    connect api with menu page

--- actions/menu.js
+++ actions/menu.js
@@ -7,7 +7,7 @@ export async function getMenu(pageNumber = 1) {
   try {
     // const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     if (!apiUrl) throw new Error('API URL is not defined');
-    const res = await fetch(`${apiUrl}/v1/menu?page=${pageNumber}`, {
+    const res = await fetch(`${apiUrl}/menu?page=${pageNumber}`, {
       cache: 'no-store',
       headers: { 'Accept': 'application/json' },
     });
