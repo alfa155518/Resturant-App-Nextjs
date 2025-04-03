66751ab0270af9c0209d045630e8c0396fe325c0
Ahmed Hassop <Ahmed Hassob>
2025-04-03 20:59:33 +0200

    connect api with menu page

--- actions/menu.js
+++ actions/menu.js
@@ -7,19 +7,19 @@ export async function getMenu(dishesNumber = 1) {
   try {
     const res = await fetch(`${apiUrl}/menu?page=${dishesNumber}`, {
       next: { 
-        revalidate: 60 // Revalidate cache every minute
+        revalidate: 5 // Revalidate cache every minute
       }
     });
     
     if (!res.ok) {
       console.error(`Error fetching menu: ${res.status} ${res.statusText}`);
-      return { error: true, status: res.status, message: 'Failed to fetch menu data' };
+      return { error:  res };
     }
     
     const data = await res.json();
     return data;
   } catch (error) {
     console.error('Menu fetch error:', error);
-    return { error: true, message: 'An error occurred while fetching menu data' };
+    return { error: error };
   }
 }
